2922a5f5e9bfbf17be1a4429f8eb609c1fe3341a
Diego Urbina <00544424@uca.edu.sv>
2025-07-08 20:25:38 -0600

    feat: add roles to endpoints

--- src/main/java/com/uca/parcialfinalncapas/controller/TicketController.java
+++ src/main/java/com/uca/parcialfinalncapas/controller/TicketController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,13 +21,14 @@ import org.springframework.web.bind.annotation.*;
 public class TicketController {
     private TicketService ticketService;
 
+    @PreAuthorize("hasRole('TECH')")
     @GetMapping
     public ResponseEntity<GeneralResponse> getAllTickets() {
         return ResponseBuilderUtil.buildResponse("Tickets obtenidos correctamente",
                 ticketService.getAllTickets().isEmpty() ? HttpStatus.BAD_REQUEST : HttpStatus.OK,
                 ticketService.getAllTickets());
     }
-
+    @PreAuthorize("hasAnyRole('TECH', 'USER')")
     @GetMapping("/{id}")
     public ResponseEntity<GeneralResponse> getTicketById(@PathVariable Long id) {
         TicketResponse ticket = ticketService.getTicketById(id);
@@ -36,12 +38,14 @@ public class TicketController {
         return ResponseBuilderUtil.buildResponse("Ticket found", HttpStatus.OK, ticket);
     }
 
+    @PreAuthorize("hasAnyRole('TECH', 'USER')")
     @PostMapping
     public ResponseEntity<GeneralResponse> createTicket(@Valid @RequestBody TicketCreateRequest ticket) {
         TicketResponse createdTicket = ticketService.createTicket(ticket);
         return ResponseBuilderUtil.buildResponse("Ticket creado correctamente", HttpStatus.CREATED, createdTicket);
     }
 
+    @PreAuthorize("hasRole('TECH')")
     @PutMapping
     public ResponseEntity<GeneralResponse> updateTicket(@Valid @RequestBody TicketUpdateRequest ticket) {
         TicketResponse updatedTicket = ticketService.updateTicket(ticket);
